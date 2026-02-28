22814012a8d64fb0412b225b23e9869ba1f4fde2
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-28 20:39:59 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/saola "1.0.1"
+(defproject org.clojars.jj/saola "1.0.2-SNAPSHOT"
   :description "Soala is a dependency injection and lifecycle management library for clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
